f26b293d1172c9d4408e5473994c7fd6d4ec22d8
Sarthak Upadhyay <u.sarthak2005@gmail.com>
2025-09-23 21:04:02 +0530

    spikes added

--- bounce.js
+++ bounce.js
@@ -2,60 +2,132 @@ let canvas = document.querySelector('canvas');
 let c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-
-
-let ball = {
-    x: 50,
-    y: canvas.height - 20,
-    radius: 20,
-    dx: 0,
-    dy: 0,
-    speed: 4,
-    gravity: 0.5,
-    jumpPower: -15,
-    right: false,
-    left: false,
-    onGround: false
+let spikes = {
+    width: 20,
+    height: 30,
+    count: 100,
+    draw: function() {
+        c.fillStyle = 'black';
+        for (let i = 0; i < this.count; i++) {
+            let x = i * this.width;
+            c.beginPath();
+            c.moveTo(x, innerHeight);             
+            c.lineTo(x + this.width / 2, innerHeight - this.height);
+            c.lineTo(x + this.width, innerHeight); 
+            c.closePath();
+            c.fill();
+        }
+    }
 };
 
-let platform = {
-    x: 110,
-    y: 500,
-    width: 150,
-    height: 20
+class plateform
+{
+    constructor(x,y)
+    {
+        this.x=x
+        this.y=y
+        this.width=Math.random()*40+100;
+        this.height=20;
+    }
+    make()
+    {   c.fillStyle='rgba(0, 255, 0, 1)';
+        c.fillRect(this.x,this.y,this.width,this.height);
+    }
 };
-let platform2 = {
-    x: 220,
-    y:300,
-    width: 100,
-    height: 20
+let p1=new plateform(110,500);
+class Ball {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = 20;
+        this.velocityin_y = 18;
+        this.gravity = 0.5;
+        this.velocityin_x = 0.5;
+        this.acceleration = 0.1;
+        this.rightarrow = false;
+        this.leftarrow = false;
+        this.uparrow=false;
+    }
+    draw() {
+        c.strokeStyle = "#e80404ff";
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        c.stroke();
+        c.fillStyle = "#e80404ff";
+        c.fill();
+    }
+    move_upward() {
+       c.clearRect(0, 0, innerWidth, innerHeight - 30); 
+
+        
+        if (this.velocityin_y <0&&this.y>=innerHeight-this.radius) {
+            this.draw();
+            p1.make();
+            this.y=innerHeight-this.radius;
+            return;
+        }
+        if(this.y+this.radius>=p1.y&&this.y+this.radius<=p1.y+p1.height&&this.velocityin_y<0&&this.x>=p1.x&&this.x<=p1.x+p1.width)
+        {    
+            this.velocityin_y=0;
+        }
+        console.log(this.velocityin_y);
+        this.y -= this.velocityin_y; this.velocityin_y -= this.gravity;
+        this.draw();
+        p1.make();
+        requestAnimationFrame(this.move_upward.bind(this));
+    }
+    jump() { 
+        if(this.y!=p1.y)
+        this.y = innerHeight;
+        this.velocityin_y = 18;
+
+        this.move_upward();
+    }
+
+    operate() {
+        
+        if (this.leftarrow == true) {
+            if (this.x > 50) {
+                this.x -= this.velocityin_x;
+                this.velocityin_x += this.acceleration;
+            }}
+            if (this.rightarrow == true) {
+                   //c.clearRect(0,0,innerWidth,innerHeight);
+                if (this.x < innerWidth - 300) {
+                    this.x += this.velocityin_x;
+                    this.velocityin_x += this.acceleration;
+                }
+            
+            } 
+            c.clearRect(0, 0, innerWidth, innerHeight);
+            this.draw();
+            p1.make();
+            spikes.draw()
+requestAnimationFrame(this.operate.bind(this));
+        
+    }
+};
+
+let ball = new Ball(50, innerHeight - 20);
+ball.draw();
+addEventListener("keydown", () => {
+    switch (event.key) {
+        case "ArrowRight": ball.rightarrow = true; break;
+        case "ArrowLeft": ball.leftarrow = true; break;
+        case "ArrowUp":if(ball.y>=innerHeight-ball.radius) ball.jump(); 
+    }
+});
+  ball.operate();
+
+addEventListener("keyup", () => {
+    switch (event.key) {
+        case "ArrowRight": ball.rightarrow = false; ball.velocityin_x=0.1;break;
+        case "ArrowLeft": ball.leftarrow = false;  ball.velocityin_x=0.1;break;
+    
+    }
 }
-function drawBall() {
-    c.fillStyle = "#e80404ff";
-    c.beginPath();
-    c.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
-    c.fill();
-    c.closePath();
-}
 
-function drawPlatform() {
-    c.fillStyle = 'rgba(0, 255, 0, 1)';
-    c.fillRect(platform.x, platform.y, platform.width, platform.height);
-
-    c.fillStyle = 'rgba(0, 255, 0, 1)';
-    c.fillRect(platform2.x, platform2.y, platform2.width, platform2.height);
-}
-
-
-
-function animate(){
-    drawBall();
-    drawPlatform();
-    requestAnimationFrame(animate);
-}
-animate()
-
-
+);
 
 
 
